b99f14ed991962eeb0130c82428f8818a5d11b08
Bryan Veloso <bryan@velo.so>
2022-09-08 09:51:02 -0700

    Start writing the service thing.

--- packages/backend/src/twitch/twitch.service.ts
+++ packages/backend/src/twitch/twitch.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 
 @Injectable()
-export class TwitchService {}
+export class TwitchService {
+  private readonly logger = new Logger(TwitchService.name)
+}
